90700142a1899667756f1331069edc6c5e6be1a1
fazle <ryan7998@gmail.com>
2021-06-19 03:43:03 -0400

    top section completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import {headShotObj, assets} from './utils/headshot-mapper';
 
 function App() {
+  
+  const [playersInfo, setPlayersInfo] = useState();
+
+  const fetchPlayers = async() =>{
+    // use https://cors-anywhere.herokuapp.com/ to resolve cors issue from localhost:
+    const response = await fetch('https://cors-anywhere.herokuapp.com/https://playline-dev-test.s3-us-west-2.amazonaws.com/playline-test.json');
+    const {players} = await response.json();
+    setPlayersInfo(players);
+
+    // players.map(player=>{
+    //   let imgSrc = player.last_name.toLowerCase();
+    //   // console.log((headShotObj[imgSrc]));
+    //   // player.img= headShotObj[imgSrc];
+    // })
+    // console.log(players);
+    // return players;
+  }
+  useEffect(()=>{fetchPlayers()}, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <img src={assets.logo} alt="logo" />
       </header>
-    </div>
+      <main>
+        <img className="progress-bar" src={assets.progressbar} />
+        <section className="top">
+          <h1>YOUR PLAYLINE IS SET!</h1>
+          <h2>COME BACK @ 7:30PM TO TRACK IT LIVE!</h2>
+          <div className="border" />
+          <p>Pro Tip: You can manage your PlayLine's until they go live in the Upcoming area</p>
+        </section>
+        <section className="players-list">
+          {playersInfo && headShotObj &&
+            playersInfo.map(player=>(
+              <div key = {`${player.last_name}-${player.points}`}>
+                {player.last_name.toLowerCase()} {player.points} 
+                {/* <img src={headShotObj.[player.last_name.toLowerCase()]}></img> */}
+              </div>
+            ))
+          }
+        </section>
+        <section className="bottom">
+
+        </section>
+      </main>
+      <footer>
+
+      </footer>
+      
+    </>
   );
 }
 
---
+++ src/utils/headshot-mapper.js
@@ -0,0 +1,15 @@
+// import gordon from "../../assets/headshots/Ar";
+import gordon from "../assets/headshots/Aaron Gordon -PLP59D7092156491v2.png";
+import davis from "../assets/headshots/Anthony-Davis-PLP59D70938C0B3Ev2.png";
+import russell from "../assets/headshots/Dangelo Russell PLP59D709255D994v4.png";
+import leonard from "../assets/headshots/Kawhi-Leonard-PLP59D7093A107E7v2.png";
+import lowry from "../assets/headshots/Kyle-Lowry-PLP59D70927DF104.png";
+import james from "../assets/headshots/Lebron-James-PLP59D7092A6F6CEv2.png";
+import siakam from "../assets/headshots/Pascal-Siakam-PLP59D70927CC6DD.png";
+
+import progressbar from "../assets/icons/progress@2x.png"
+import logo from "../assets/logo/logo@1x.png";
+
+export const headShotObj = {gordon:gordon, davis:davis, russell:russell, leonard:leonard, lowry:lowry, james:james, siakam:siakam}
+
+export const assets = {logo, progressbar};
